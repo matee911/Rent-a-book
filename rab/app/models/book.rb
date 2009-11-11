6678de98ca75e16ed998b9b1360c55363862da16
Arkadiusz Oleksy <arkadiusz@macbook-arkadiusz-oleksy.local>
2009-11-12 00:12:31 +0100

    isbn validators

--- rab/app/models/book.rb
+++ rab/app/models/book.rb
@@ -2,6 +2,9 @@ require 'mongo_mapper'
 
 class Book
   include MongoMapper::Document
+  include Validatable
+  
+  validate :validate_isbn
 
   key :title, String
   key :description, String
@@ -10,6 +13,39 @@ class Book
   key :shop_url, String
   key :isbn, String
   # tags (python, ruby, iphone, management etc.)?
+  
+  
+  private
+    def validate_isbn
+      # isbn validators http://en.wikipedia.org/wiki/Isbn
+      isbn_arr = isbn.gsub(/[^\d]/,"").split("")
+      unless (isbn_arr.length == 10 || isbn_arr.length == 13)
+        errors.add(:isbn, "wrong isbn number")
+      else
+        self.send("validate_isbn#{isbn_arr.length}", isbn_arr)
+      end
+    end
+    
+    def validate_isbn10(isbn_arr)
+      check_value = isbn_arr.delete_at(-1).to_i
+      checksum = 0
+      isbn_arr.each_with_index { |n,i| checksum += (i+1) * n.to_i }
+      errors.add(:isbn, "wrong isbn10 checksum") unless check == (checksum % 11)
+    end
+    
+    def validate_isbn13(isbn_arr)
+      check_value = isbn_arr.delete_at(-1).to_i
+      checksum = 0
+      isbn_arr.each_with_index do |n,i|
+        if i%2 == 0
+          checksum += n.to_i
+        else
+          checksum += 3 * n.to_i
+        end
+      end
+      errors.add(:isbn, "wrong isbn13 chcecksum") unless (10 - checksum % 10) == check_value
+    end
+  
 
 #  http://github.com/jnunemaker/validatable
 #  validates_presence_of :title, :status
