8fc956cbcb41123acd0bd336030b9c5df8d3943e
Arkadiusz Oleksy <arkadiusz@macbook-arkadiusz-oleksy.local>
2009-11-15 23:50:04 +0100

    refactored and fixed (i hope) isbn validation

--- rab/app/models/book.rb
+++ rab/app/models/book.rb
@@ -7,6 +7,9 @@ class Book
   include MongoMapper::Document
   include Validatable
   
+  ISBN10_RX = /^(?:\d[\ |-]?){9}[\d|X]$/
+  ISBN13_RX = /^(?:\d[\ |-]?){13}$/
+  
   STATUS_TYPES = {
     :na => [0, "Not available"],
     :proposition => [1, "Proposition"],
@@ -63,6 +66,38 @@ class Book
       # doc.xpath('/entry/creator').text
       hint
     end
+    
+    def valid_isbn13?(isbn_str)
+      if isbn_str.match(ISBN13_RX)
+        isbn_arr = isbn_str.upcase.gsub(/\ |-/, '').split('')
+        check_value = isbn_arr.pop.to_i
+        checksum = 0
+        isbn_arr.each_with_index do |n, i|
+          m = (i % 2 == 0) ? 1 : 3
+          checksum += m * n.to_i
+        end
+        res = (10 - (checksum % 10))
+        res = 0 if res == 10
+
+        res == check_value
+      else
+        false
+      end
+    end
+    
+    def valid_isbn10?(isbn_str)
+      if isbn_str.match(ISBN10_RX)
+        isbn_arr = isbn_str.upcase.gsub(/\ |-/, '').split('')
+        check_value = isbn_arr.pop
+        check_value = (check_value == 'X') ? 10 : check_value.to_i
+        checksum = 0
+        isbn_arr.each_with_index { |n, i| checksum += (i + 1) * n.to_i }
+        (checksum % 11) == check_value
+      else
+        false
+      end
+    end
+    
   end
   
   def status_readable
@@ -128,37 +163,26 @@ class Book
   private
     def validate_isbn
       # isbn validators http://en.wikipedia.org/wiki/Isbn
-      isbn_arr = isbn.gsub(/[^\d]/,"").split("")
-      unless (isbn_arr.length == 10 || isbn_arr.length == 13)
-        errors.add(:isbn, "wrong isbn number")
+      if isbn.match(ISBN10_RX)
+        validate_isbn10
+      elsif isbn.match(ISBN13_RX)
+        validate_isbn13
       else
-        self.send("validate_isbn#{isbn_arr.length}", isbn_arr)
+        errors.add(:isbn, "wrong isbn number")
       end
     end
     
-    def validate_isbn10(isbn_arr)
-      check_value = isbn_arr.delete_at(-1).to_i
-      checksum = 0
-      isbn_arr.each_with_index { |n,i| checksum += (i+1) * n.to_i }
-      errors.add(:isbn, "wrong isbn10 checksum") unless check_value == (checksum % 11)
+    def validate_isbn10
+      errors.add(:isbn, "wrong isbn10 checksum") unless Book.valid_isbn10?(isbn)
     end
     
-    def validate_isbn13(isbn_arr)
-      check_value = isbn_arr.delete_at(-1).to_i
-      checksum = 0
-      isbn_arr.each_with_index do |n,i|
-        if i%2 == 0
-          checksum += n.to_i
-        else
-          checksum += 3 * n.to_i
-        end
-      end
-      errors.add(:isbn, "wrong isbn13 chcecksum") unless (10 - checksum % 10) == check_value
+    def validate_isbn13
+      errors.add(:isbn, "wrong isbn13 chcecksum") unless Book.valid_isbn13?(isbn)
     end
     
     def create_slug
       if self.title
-        self.slug = "#{self.isbn.gsub(/[^\d]/,"")}-#{self.title.to_slug}"
+        self.slug = "#{self.isbn.gsub(/\ |-/, '')}-#{self.title.to_slug}"
       end
     end
 
