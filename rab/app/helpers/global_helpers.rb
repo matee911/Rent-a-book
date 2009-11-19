23b925e0a0f3c47b29ab4a71b39dfb003b4f94e9
matee <matee@matee.net>
2009-11-19 21:51:17 +0100

    date helper;

--- rab/app/helpers/global_helpers.rb
+++ rab/app/helpers/global_helpers.rb
@@ -1,5 +1,9 @@
 module Merb
   module GlobalHelpers
-    # helpers defined here available to all views.
+
+    def readable_date(date)
+      date.strftime_ordinalized('%b %d, %Y  %H:%M') unless date.nil?
+    end
+
   end
 end
